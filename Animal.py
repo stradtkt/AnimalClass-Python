045bd41caf689189d0635d8a6d04e52a965c7142
Kevin <stradtkt22@gmail.com>
2018-06-27 19:55:10 -0400

    Added more to the Animal Class and subclasses

--- Animal.py
+++ Animal.py
@@ -18,12 +18,30 @@ class Animal(object):
     return self
 
 class Dog(Animal):
-  def __init__(self, name, health):
-    super(Dog, self).__init__()
+  def __init__(self, name, health=150):
+    super(Dog, self).__init__(name, health)
   def pet(self):
     self.health += 5
     print "Health after petting: " + str(self.health)
     return self
 
-dog_1 = Animal("Buster", 150)
+dog_1 = Dog("Buster")
 dog_1.walk().walk().walk().run().run().pet().display_health()
+
+class Dragon(Animal):
+  def __init__(self,name,health=170):
+    super(Dragon, self).__init__(name, health)
+  
+  def fly(self):
+    self.health -= 10
+    return self
+
+  def display_health(self):
+    super(Dragon, self).display_health()
+    print "I am a Dragon"
+dragon_1 = Dragon("Optimus")
+dragon_1.fly().fly().display_health()
+
+# Below will produce an error
+# animal_1 = Animal("Buddy", 100)
+# animal_1.pet().fly()
